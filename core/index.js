8eabddd49c40b1189a4dced9ee213a3173c2ee30
lithiumjs <lithium@lithium-js.pro>
2017-10-22 20:56:55 +0200

    I don't really remember what have I done here

--- core/index.js
+++ core/index.js
@@ -4,16 +4,21 @@ process.on("uncaughtException", function(e) {
 
 var Message = require("./message.js");
 var Command = require("./command.js");
+var https = require("https");
+var Image = require("image-binary");
 
 var Core = function(options) {
 	console.log("Initializing core...");
 
-	this.vk = require("VK-Promise")(options.access_token);
+	this.VK = require("VK-Promise")
+	this.vk = new this.VK(options.access_token);
 	this.commands = [];
 	this.ignore = [];
 	this.admin_id = options.admin_id;
 	this.admin_only = options.admin_only;
 	this.mentions = options.mentions;
+	this.mps = {};
+	client = require("rucaptcha-client").create(options.rucaptcha_key);
 
 	var self = this;
 
@@ -27,14 +32,27 @@ var Core = function(options) {
 
 	console.log("LongPoll initialized (hopefully)");
 
+	setInterval(function() {
+		self.mps = {};
+	}, 1000);
+
 	this.vk.on("message", function(event, plain_message) {
 		var message = new Message(plain_message);
+
+		if (message.out) return;
 
 		if (self.admin_only && message.from_id != self.admin_id) return;
 
 		if (self.ignore.indexOf(message.from_id) > -1) return;
 
 		if ((self.mentions.indexOf(message.mention.toLowerCase() + ",") == -1) && (self.mentions.indexOf(message.mention.toLowerCase()) == -1)) return;
+
+		self.mps[message.from_id] = (self.mps[message.from_id] || 0) + 1;
+
+		if (self.mps[message.from_id] > 4) {
+			message.reply("Вы были забанены за флуд, а ибо нефиг.", {attachment: "video71358147_456239101"});
+			return self.ignore.push(message.from_id);
+		}
 
 		var command = self.commands.find(function (cmd) {
 			return cmd.variations.indexOf(message.cmd.toLowerCase()) > -1;
@@ -52,6 +70,29 @@ var Core = function(options) {
 			console.log(e.stack);
 		}
 	});
+
+	this.vk.on("captcha", function(event, data) {
+		let balance;
+		client.balance
+		.then((num) => {
+			balance = num;
+			return Image.create(data.captcha_img);
+		})
+		.then((image) => {
+			client.image = image;
+			return client.solve({});
+		})
+		.then((answer) => {
+			console.log(
+				'====== Captcha Solver ======' + '\n' +
+				'Balance: ' + balance + '\n' +
+				'Answer: ' + answer.text + '\n' +
+				'============================'
+			);
+			data.submit(answer.text);
+		});
+	});
+
 	console.log("Core initialized, awaiting commands");
 };
 
